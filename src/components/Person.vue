a399ecea34100e79bb9ec7271a0269828a9e228f
adam5096 <adamchou5096@gmail.com>
2025-04-23 15:41:32 +0800

    011_ref創建基本類型的響應式數據

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -10,76 +10,34 @@
     </div>
 </template>
 
-<!-- <script lang="ts">
-// name 會被記錄在 vue 的 devtools 中 (可點開瀏覽器開發工具查看)
-    export default {
-        name: 'Person123',
-        // setup() {
-            // setup 函數中的 this 是 undefined，vue 3 中已經弱化了 this
-
-            // 數據
-            // let name = ('哈哈')
-            // let age = (18)
-            // let gender = ('男')
-            // let tel = ('12345678901')
-
-
-            // 函數
-            // 這樣修改 name 不會觸發響應式
-            // name 確實改變了，但畫面不會變
-            // function changeName() {
-            //     console.log('@@ changeName fired')
-            //     name = '小明'
-            //     console.log(name)
-            // }
-            // function changeAge() {
-            //     console.log('@@ changeAge fired')
-            //     age += 1
-            //     console.log(age)
-            // }
-            // function showTel() {
-            //     console.log('@@ showTel fired')
-            //     alert(tel)
-            //     console.log(tel)
-            // }
-            // 將數據、函數交出去，模板中才能使用
-            // return {
-            //     name,
-            //     age,
-            //     gender,
-            //     tel,
-            //     changeName,
-            //     changeAge,
-            //     showTel
-            // }
-        // }
-    }
-</script> -->
-
 <!-- vite-plugin-vue-setup-extend 插件 -->
 <!-- 支持在 setup 中加入 name 屬性來設置元件名稱-->
 
-<script setup lang="ts">
-    // setup 函數中的 this 是 undefined，vue 3 中已經弱化了 this
+<script setup lang="ts" name="PersonTest">
+import { ref } from 'vue'
     // 數據
-    let name = ('哈哈')
-    let age = (18)
+    let name = ref('哈哈')
+    let age = ref(18)
     let gender = ('男')
     let tel = ('12345678901')
 
+    console.log(1, name)
+    console.log(2, age)
+    console.log(3, gender)
+    console.log(4, tel)
 
     // 函數
     // 這樣修改 name 不會觸發響應式
     // name 確實改變了，但畫面不會變
     function changeName() {
         console.log('@@ changeName fired')
-        name = '小明'
-        console.log(name)
+        name.value = '小明'
+        console.log(name.value)
     }
     function changeAge() {
         console.log('@@ changeAge fired')
-        age += 1
-        console.log(age)
+        age.value += 1
+        console.log(age.value)
     }
     function showTel() {
         console.log('@@ showTel fired')
