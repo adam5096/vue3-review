c275cc59afddc569663d93eb68f6f528140d4157
adam5096 <adamchou5096@gmail.com>
2025-04-25 16:39:10 +0800

    14_watch_02

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="person">
-    <h1>情況一: 監聽 [ref] 基本類型數據</h1>
-       <h2>當前加總: {{ sum }}</h2>
-       <button @click="changeSum">點我 sum + 1</button>
+    <h1>情況二: 監聽 [ ref ] 定義的物件類型數據</h1>
+       <h2>姓名: {{ person.name }}</h2>
+       <h2>年齡: {{ person.age }}</h2>
+       <button @click="changeName">修改名字</button>
+       <button @click="changeAge">修改年紀</button>
+       <button @click="changepPerson">修改整個人</button>
     </div>
 
 </template>
@@ -10,19 +13,40 @@
 <script setup lang="ts" name="PersonTest">
     import { ref, watch } from 'vue'
     // 數據
-    let sum = ref(0)
-
-    const changeSum = () => {
-        sum.value++
-    }
-
-    // 監聽
-    const stopWatch = watch(sum, (newValue, oldValue) => {
-        console.log('sum 的值變了', newValue, oldValue)
-        if (newValue >= 10) {
-            stopWatch()
-        }
+    let person = ref({
+        name: 'adam',
+        age: 18
     })
+    // 數據 END
+
+    // 方法
+    const changeName = () => {
+        person.value.name += " ~"
+    }
+    const changeAge = () => {
+        person.value.age += 1
+    }
+    const changepPerson = () => {
+        person.value = {
+            name: 'chou',
+            age: 90
+        }
+    }
+    // 方法 END
+
+
+    // A: watch
+    // 直接監聽 person, 無論是修改  person.value.name 或  person.value.age 並不會觸發 watch 執行
+    // 反而 修改 person 整個物件時, 會觸發 watch 執行
+
+    // B: watch
+    // deep: true : 深度監聽, 監聽 person 整個物件
+    // immediate: true ; 頁面準備好的第一時間，就立即執行一次
+    // wacth 第 1 參數: 監視的數據, 第 2 參數: callback 函數
+    watch(person, (newValue, oldValue) => {
+        console.log('person 的值變了', newValue, oldValue)
+    }, { deep: true , immediate: true })
+    // watch END
 </script>
 
 <style lang="css" scoped>
