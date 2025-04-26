be7f20c9b5d7bb807c9e851bb8794d25493cb4c6
adam5096 <adamchou5096@gmail.com>
2025-04-27 01:04:15 +0800

    025_props_的使用

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-    <Person />
+
+    <Person :personList="personList" />
 
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+    import { defineOptions ,defineComponent } from 'vue';
     import Person from './components/Person.vue'
-    export default {
-        // 組件名稱
-        name: 'App',
-        // 組件註冊
+
+
+    defineOptions({
+        name: 'App'
+    })
+
+    defineComponent({
         components: {
             Person
         }
-    }
+    })
+
+    let personList = [
+        {
+            id: 1,
+            name: '張三',
+            age: 18
+        },
+        {
+            id: 2,
+            name: '李四',
+            age: 20
+        },
+        {
+            id: 3,
+            name: '王五',
+            age: 22
+        }
+    ]
 
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,66 +1,34 @@
 <template>
     <div class="person">
-    <h2>當前水溫度: {{ temp }}</h2>
-    <h2>當前水高度: {{ hright }}</h2>
-    <button @click="addTemp">點我水溫加加 10</button>
-    <button @click="addHright">點我水高度加 10</button>
-    <hr>
-    <h2>{{ watchEffectCallback }}</h2>
-    
+
+    <ul>
+        <li v-for="person in props.personList" :key="person.id">
+            <h2>姓名: {{ person.name }}</h2>
+            <h2>年齡: {{ person.age }}</h2>
+        </li>
+    </ul>
     </div>
 
 </template>
 
 <script setup lang="ts" name="PersonTest">
-import { ref, watchEffect, watch, onMounted } from 'vue'
-    // ********** 數據 **********
-    let temp = ref(10)
-    let hright = ref(0)
-    // ********** 數據 END **********
+import { ref, onMounted } from 'vue'
 
-    // ********** 方法 **********
-    const addTemp = () => {
-        temp.value+= 10
-    }
-    const addHright = () => {
-        hright.value+= 10
-    }
-    // ********** 方法 END **********
+defineOptions({
+    name: 'Person'
+})
+    
+    // defineProps(['personList'])
 
-    // ********** 需求: 監聽水溫度或水高度, 當水溫度過高或水高度過高時, 發送網路請求 **********
-    // watch 寫法
-    // watch([temp, hright], (newValue) => {
-    //     let [newTemp, newHeight] = newValue
-
-    //     if(newTemp > 40 || newHeight > 100){
-    //         console.log('水溫度過高或水高度過高')
-    //         console.log(newTemp, newHeight)
-    //         console.log('網路請求發送中...')
-    //     }
-    // })
-
-    // watchEffect 寫法
-
-    let watchEffectCallback = watchEffect(() => {
-            if(temp.value > 40 || hright.value > 100){
-            console.log('水溫度過高或水高度過高')
-            console.log(temp.value, hright.value)
-            console.log('網路請求發送中...')
-        }
+    // 限制來源 + 限制類型 + 限制必要 + 限制預設值
+    const props = withDefaults(defineProps<{
+        personList: {id: number, name: string, age: number}[]
+    }>(), {
+        personList: () => [{id: 999, name: 'DEFAULT', age: 999}]
     })
 
-    // watch END
 
-    // ********** watchEffect 與 watch 的區別 **********
-    // 使用 watch: 需要寫明白監視哪一個(或多個)響應數據
-    // 使用 watchEffect: 自動監視 callback 中所有提到的響應數據
 
-    
-    // 理解 watch: 相當於給任何響應式數據新增事件監聽 + callback
-    //  適合放入 watch 的場景:
-    // 1.操作 DOM
-    // 2.網路請求
-    // 3.滿足特定條件或發生特定事件後的 callback
 </script>
 
 <style lang="css" scoped>
