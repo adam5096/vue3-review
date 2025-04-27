a1cf3d9444e7ed2d2b03db801daa2332c167a3f7
adam5096 <adamchou5096@gmail.com>
2025-04-27 13:02:00 +0800

    028 Vue3的生命週期

---
+++ backup/025_props_的使用/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+    <Person :personList="personList" />
+
+</template>
+
+<script setup lang="ts">
+    import { defineOptions ,defineComponent } from 'vue';
+    import Person from './components/Person.vue'
+
+
+    defineOptions({
+        name: 'App'
+    })
+
+    defineComponent({
+        components: {
+            Person
+        }
+    })
+
+    let personList = [
+        {
+            id: 1,
+            name: '張三',
+            age: 18
+        },
+        {
+            id: 2,
+            name: '李四',
+            age: 20
+        },
+        {
+            id: 3,
+            name: '王五',
+            age: 22
+        }
+    ]
+
+</script>
---
+++ backup/025_props_的使用/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+
+    <ul>
+        <li v-for="person in props.personList" :key="person.id">
+            <h2>姓名: {{ person.name }}</h2>
+            <h2>年齡: {{ person.age }}</h2>
+        </li>
+    </ul>
+    </div>
+
+</template>
+
+<script setup lang="ts" name="PersonTest">
+import { ref, onMounted } from 'vue'
+
+defineOptions({
+    name: 'Person'
+})
+    
+    // defineProps(['personList'])
+
+    // 限制來源 + 限制類型 + 限制必要 + 限制預設值
+    const props = withDefaults(defineProps<{
+        personList: {id: number, name: string, age: number}[]
+    }>(), {
+        personList: () => [{id: 999, name: 'DEFAULT', age: 999}]
+    })
+
+
+
+</script>
+
+<style lang="css" scoped>
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px ;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin:0 5px;
+}
+
+li {
+    font-size: 20px;
+}
+</style>
