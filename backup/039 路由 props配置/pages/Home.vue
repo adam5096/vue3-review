a2b336d2f2e0c4c6ecf0ade47e1c31b95209bbeb
adam5096 <adamchou5096@gmail.com>
2025-04-28 22:43:39 +0800

    039 路由 props配置

---
+++ backup/039 路由 props配置/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>首頁</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineOptions } from 'vue';
+
+defineOptions({
+    name: 'Home'
+})
+
+</script>
+
+<style scoped>
+
+</style>
